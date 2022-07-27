005605dbaa38bea7fb3d0d6b8b7f14ce2277f6ab
Bailey Cage <bailey.cage@gmail.com>
2022-07-27 15:10:01 -0400

    update content in index.html

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
 // document.querySelector("body").appendChild(h2);
-
-// const mainNav = document.getElementsByClassName("main-nav");
-// let mainNavElementWidth = 0;
-// for (let i=0; i < mainNav.length; ++i) {
-//     if (mainNav[i].clientWidth > mainNavElementWidth) {
-//         mainNavElementWidth = mainNav[i].clientWidth;
-//     }
-// }
-// for (let ele of mainNav) {
-//     ele.style.width = mainNavElementWidth;
-// }
